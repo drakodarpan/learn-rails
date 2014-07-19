dd2c485a8c66ee0866e22abdfc6f6c789d50a510
drakodarpan <drako_darpan@prodigy.net.mx>
2014-07-18 23:26:42 -0600

    contact form
    Using simple form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,6 @@
 class ContactsController < ApplicationController
   
+=begin  
   def process_form
     if params[:contact][:name].blank?
       raise 'Name is blank!!!'
@@ -13,5 +14,28 @@ class ContactsController < ApplicationController
     message = "Received request from #{params[:contact][:name]}"
     redirect_to root_path, :notice => message
   end
+=end
+
+  def new
+    @contact = Contact.new
+  end
   
+  def create
+    @contact = Contact.new(secure_params)
+    if @contact.valid?
+      # TODO save data
+      # TODO send message
+      flash[:notice] = "Message sent from #{@contact.name}."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+    def secure_params
+      params.require(:contact).permit(:name, :email, :content)
+    end
+
 end
